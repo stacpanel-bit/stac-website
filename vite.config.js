87d7938716808704afe00fd92c53dc551ea869ce
stacpanel-bit <your-new-email@example.com>
2026-02-27 23:31:56 +0100

    fix: corrected vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/STAC-WEB/',
+  base: '/stac-website/',   // ✅ must match repo name exactly
   plugins: [react()],
 })
